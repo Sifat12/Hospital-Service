c437acaa6a8f5188e7eb09763c906c5b0c046b48
Sifat12 <38453547+Sifat12@users.noreply.github.com>
2019-07-04 17:24:28 +0600

    homepage

---
+++ home.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Homepage</title>
+    </head>
+    <body>
+        <div class="header">
+           <table border="1" width="100%" height="10%" >
+				<tr>
+					<td><center><a href="" style="text-decoration: none;">Home</a></center></td>
+					<td><center><a href="" style="text-decoration: none;">Service</a></center></td>
+					<td><center><a href="" style="text-decoration: none;">Contact</a></center></td>
+					<td><center><a href="" style="text-decoration: none;">About</a></center></td>
+					<td><center><a href="" style="text-decoration: none;">Login</a></center></td>
+					<td><center><a href="" style="text-decoration: none;">Register</a></center></td>
+					
+				</tr>
+				
+				
+			</table>
+        </div>
+    </body>
+</html>
